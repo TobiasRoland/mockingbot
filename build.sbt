d46b53ad1e9cc65e950bbab7e1dab69692947a70
TobiasRoland <TobiasRoland>
2020-01-30 04:20:07 +0000

    But hey, it's workin'

--- build.sbt
+++ build.sbt
@@ -12,10 +12,5 @@ libraryDependencies += ScalablyTyped.Q.qs
 libraryDependencies += ScalablyTyped.S.`safe-json-stringify`
 libraryDependencies += "org.scalatest" %%% "scalatest" % "3.1.0" % "test"
 
-
-/* If the library is typed up in `DefinitelyTyped` (has `-dt-` in the version string) you'll also need this.
- * The reason why is that versions strings there are just comments, and frequently wrong. Automatically including
- *  that would break your build.
- */
 npmDependencies in Compile += "safe-json-stringify" -> "1.1"
 npmDependencies in Compile += "qs" -> "6.9"
--- src/main/scala/codes/mostly/gcp/EntryPoint.scala
+++ src/main/scala/codes/mostly/gcp/EntryPoint.scala
@@ -15,50 +15,49 @@ import scala.scalajs.js.{Function2 => JSFunction2}
 
 object EntryPoint {
 
-  val slackSecret = "" //TODO
-  val headerTimestamp = "x-slack-request-timestamp"
+  val slackSecret          = ""
+  val headerTimestamp      = "x-slack-request-timestamp"
   val headerSlackSignature = "x-slack-signature"
 
   @JSExportTopLevel("HelloWorld")
   val Mock: JSFunction2[ExpressRequest, Response, Unit] = (req, res) ⇒ {
-    val request = req.asInstanceOf[Request]
-    val signature = parseRequestSignature(request)
-//    val validation = SlackRequestValidator.validate(slackSecret, requestSignature)
-    res.status(200).send("" + signature.toString)
+    val request    = req.asInstanceOf[Request]
+    val signature  = parseRequestSignature(request)
+    val validation = SlackRequestValidator.validate(slackSecret, signature)
+    res.status(200).send("" + signature.toString + "\n\n" + validation.toString)
   }
 
-  private def parseRequestSignature(req: Request): RequestSignature = {
+  private def parseRequestSignature(req: Request): RequestSignature =
     RequestSignature(
       qsStringify(req.body, IStringifyOptions(format = RFC1738)),
       req.headers.`x-slack-request-timestamp`.toInt,
-      req.headers.`x-slack-signature`
+      req.headers.`x-slack-signature`,
     )
-  }
 
   @js.native
   trait Request extends js.Object {
     val body: SlashCommandBody = js.native
-    val headers: SlackHeaders = js.native
+    val headers: SlackHeaders  = js.native
   }
 
   @js.native
   trait SlashCommandBody extends js.Object {
-    val token: String = js.native
-    val team_id: String = js.native
-    val team_domain: String = js.native
-    val channel_id: String = js.native
+    val token: String        = js.native
+    val team_id: String      = js.native
+    val team_domain: String  = js.native
+    val channel_id: String   = js.native
     val channel_name: String = js.native
-    val user_id: String = js.native
-    val command: String = js.native
-    val text: String = js.native
+    val user_id: String      = js.native
+    val command: String      = js.native
+    val text: String         = js.native
     val response_url: String = js.native
-    val trigger_id: String = js.native
+    val trigger_id: String   = js.native
   }
 
   @js.native
   trait SlackHeaders extends js.Object {
     val `x-slack-request-timestamp`: String = js.native
-    val `x-slack-signature`: String = js.native
+    val `x-slack-signature`: String         = js.native
   }
 
 }
